2ad2dc5c81cf842c3439a0d40243a043da67cf54
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-04-15 23:33:44 +0600

    add stats chart based on timeline data

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,41 +1,54 @@
 "use client";
 import { FriendContext } from "@/Components/FriendsProvider/FriendsProvider";
 import React, { useContext } from "react";
-import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { Cell, Legend, Pie, PieChart, Tooltip } from "recharts";
 
 const StatsPage = () => {
-    const {timeline} = useContext(FriendContext)
-    console.log(timeline)
-  const data = [{
-    data: timeline.length, value: timeline.type
-  }];
+  const { timeline } = useContext(FriendContext);
+
+  const call = timeline.filter((item) => item.type === "Call");
+  const text = timeline.filter((item) => item.type === "Text");
+  const video = timeline.filter((item) => item.type === "Video");
+
+  const data = [
+    { type: "Call", count: call.length },
+    { type: "Text", count: text.length },
+    { type: "Video", count: video.length },
+  ];
   return (
-    <div className="w-7xl mx-auto py-20">
+    <div className="w-7xl mx-auto py-20 space-y-6">
       <h1 className="font-bold text-5xl">Friendship Analytics</h1>
 
-      <div>
-        <PieChart
-          style={{
-            width: "100%",
-            maxWidth: "500px",
-            maxHeight: "80vh",
-            aspectRatio: 1,
-          }}
-          responsive
-        >
+      <div className="bg-white rounded p-8 space-y-2">
+        <h4 className="font-medium text-xl">By Interaction Type</h4>
+        <PieChart width={400} height={400} className="mx-auto">
           <Pie
             data={data}
+            dataKey="count"
+            nameKey="type"
             innerRadius="80%"
             outerRadius="100%"
-            // Corner radius is the rounded edge of each pie slice
             cornerRadius="50%"
-            fill="#8884d8"
-            // padding angle is the gap between each pie slice
             paddingAngle={5}
-            dataKey="value"
-          />
-        <Tooltip />
-        <Legend/>
+          >
+            {data.map((item, index) => (
+              <Cell
+                key={index}
+                fill={
+                  item.type === "Text"
+                    ? "#7E35E1"
+                    : item.type === "Call"
+                      ? "#244D3F"
+                      : item.type === "Video"
+                        ? "#37A163"
+                        : "#ccc"
+                }
+              />
+            ))}
+          </Pie>
+
+          <Tooltip />
+          <Legend />
         </PieChart>
       </div>
     </div>
